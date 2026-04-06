4608c2a95dc3341e262827bcb0eb0e1012aceca8
Evgeniy-Sub8way <sub8way@gmail.com>
2026-04-06 22:24:49 +0300

    stars-change

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -1,67 +1,67 @@
 import 'css-star-rating/css/star-rating.css';
 
 function feedbackTemplate(feedback) {
-	const rating = feedback.rate;
-	const valueRating = Math.floor(rating);
-	const hasHalf = rating % 1 !== 0;
-	const halfClass = hasHalf ? 'half' : '';
+  const rating = feedback.rate;
+  const valueRating = Math.floor(rating);
+  const hasHalf = rating % 1 !== 0;
+  const halfClass = hasHalf ? 'half' : '';
 
-	return `<li class="feedback-item">
+  return `<li class="feedback-item">
 	<div class="rating star-svg value-${valueRating} ${halfClass} color-default">
 		<ul class="star-container">
 			<li class="star">
 				<svg class="star-empty">
-					<use href="./img/star-rating.icons.svg#star-empty"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-empty"></use>
 				</svg>
 				<svg class="star-half">
-					<use href="./img/star-rating.icons.svg#star-half"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-half"></use>
 				</svg>
 				<svg class="star-filled">
-					<use href="./img/star-rating.icons.svg#star-filled"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-filled"></use>
 				</svg>
 			</li>
 			<li class="star">
 				<svg class="star-empty">
-					<use href="./img/star-rating.icons.svg#star-empty"></use>
+					<use href="/it-creators-project-meblerij/it-creators-project-meblerija/img/star-rating.icons.svg#star-empty"></use>
 				</svg>
 				<svg class="star-half">
-					<use href="./img/star-rating.icons.svg#star-half"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-half"></use>
 				</svg>
 				<svg class="star-filled">
-					<use href="./img/star-rating.icons.svg#star-filled"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-filled"></use>
 				</svg>
 			</li>
 			<li class="star">
 				<svg class="star-empty">
-					<use href="./img/star-rating.icons.svg#star-empty"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-empty"></use>
 				</svg>
 				<svg class="star-half">
-					<use href="./img/star-rating.icons.svg#star-half"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-half"></use>
 				</svg>
 				<svg class="star-filled">
-					<use href="./img/star-rating.icons.svg#star-filled"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-filled"></use>
 				</svg>
 			</li>
 			<li class="star">
 				<svg class="star-empty">
-					<use href="./img/star-rating.icons.svg#star-empty"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-empty"></use>
 				</svg>
 				<svg class="star-half">
-					<use href="./img/star-rating.icons.svg#star-half"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-half"></use>
 				</svg>
 				<svg class="star-filled">
-					<use href="./img/star-rating.icons.svg#star-filled"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-filled"></use>
 				</svg>
 			</li>
 			<li class="star">
 				<svg class="star-empty">
-					<use href="./img/star-rating.icons.svg#star-empty"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-empty"></use>
 				</svg>
 				<svg class="star-half">
-					<use href="./img/star-rating.icons.svg#star-half"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-half"></use>
 				</svg>
 				<svg class="star-filled">
-					<use href="./img/star-rating.icons.svg#star-filled"></use>
+					<use href="/it-creators-project-meblerija/img/star-rating.icons.svg#star-filled"></use>
 				</svg>
 			</li>
 		</ul>
@@ -72,12 +72,12 @@ function feedbackTemplate(feedback) {
 }
 
 function renderFeedback(feedbackList) {
-	const container = document.querySelector('.feedback-list');
-	if (!container || !feedbackList || !feedbackList.length) return;
+  const container = document.querySelector('.feedback-list');
+  if (!container || !feedbackList || !feedbackList.length) return;
 
-	container.innerHTML = feedbackList.map(feedbackTemplate).join('');
+  container.innerHTML = feedbackList.map(feedbackTemplate).join('');
 }
 
 window.FeedbackModule = {
-	render: renderFeedback,
+  render: renderFeedback,
 };
